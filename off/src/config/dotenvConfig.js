ea80419c9e2ed528d2288eceabf05b5c712b49fc
Iago Cunha <iagocunha51@gmail.com>
2024-11-19 17:21:18 -0300

    Removendo arquivos e configurações do backend - pasta OFF

--- off/src/config/dotenvConfig.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-module.exports = {
-    jwtSecret: process.env.JWT_SECRET,
-    port: process.env.PORT || 3000
-}
--- off/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authControllers = require('../controllers/authControllers');
-
-// CRIANDO ROTA LOGIN
-router.post('/login', authControllers);
-
-
-module.exports = router;
